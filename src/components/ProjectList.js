186429230ac2a2b90f8549f94d5c0ce029eefddb
Amy Shi <amyhshi92@gmail.com>
2022-03-12 14:48:37 -0500

    commit tree a30a579ec837335a30a66386d320e8aadda83bfe

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,20 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projArray = projects.map(project => {
+    return <ProjectItem 
+      key={project.name}
+      name={project.name} 
+      about={project.about} 
+      technologies={project.technologies}
+    />
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projArray}
+      </div>
     </div>
   );
 }
